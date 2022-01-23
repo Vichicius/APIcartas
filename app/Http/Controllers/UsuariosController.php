0a999076d96c1a37de67c7f1f2fea202f9dccbf1
Vichicius <luismartinezmilla@gmail.com>
2022-01-23 15:22:15 +0100

    mas controllers

---
+++ app/Http/Controllers/UsuariosController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+use Exception;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UsuariosController extends Controller
+{
+
+    public function register(Request $req){ //Pide: nickname, email, password y rol
+        $jdata = $req->getContent();
+        $data = json_decode($jdata);
+        $response["status"]=1;
+
+        if(isset($data->nickname) && isset($data->email) && isset($data->password) && isset($data->rol)){
+            try{
+                $user = new Usuario;
+                $user->nickname = $data->nickname;
+                //check email
+                if(!preg_match("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$^", $data->email)) { 
+                    throw new Exception("Error: Email no válido");
+                }
+                $user->email = $data->email;
+                if(preg_match("/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{6,}/", $data->password)){
+                    $user->password = Hash::make($data->password);
+                }else{
+                    throw new Exception("Contraseña insegura. Mínimo: 1 Mayúscula, 1 minúscula, 1 caracter especial y 1 número");
+                }
+                $roles = ['particular', 'profesional', 'administrador'];
+
+                if(in_array($data->rol,$roles)){
+                    $user->rol = $data->rol;
+                }else{
+                    throw new Exception("Error: Rol introducido incorrecto");
+                }
+
+                $user->save();
+                $response["msg"]="Guardado con éxito";
+
+            }catch(\Exception $e){
+                $response["status"]=0;
+                $response["msg"]=$e->getMessage();
+            }
+            
+        }else{
+            $response["status"]=0;
+            $response["msg"]="introduce name, email, password y rol";
+        }
+
+        
+        return response()->json($response);
+    }
+
+    public function login(Request $req){ //Pide: nickname y password
+        $jdata = $req->getContent();
+        $data = json_decode($jdata);
+        $response["status"]=1;
+        try{
+            if(isset($data->nickname) && isset($data->password)){
+                $user = Usuario::where('nickname', $data->nickname)->first();
+                if(!isset($user)){
+                    throw new Exception("Error: Nickname no existe");
+                }
+                if(Hash::check($data->password, $user->password)){
+                    
+                    $allTokens = Usuario::pluck('api_token')->toArray();
+                    do {
+                        $user->api_token = Hash::make(now().$user->email);
+                    } while (in_array($user->api_token, $allTokens)); //En bucle mientras que el apitoken esté duplicado
+                    $user->save();
+                    $response["msg"] = "sesion iniciada correctamente";
+                    $response["api_token"] = $user->api_token;
+
+                }else{
+                    //contraseña incorrecta
+                    throw new Exception("Error: Contraseña incorrecta");
+                }
+            }
+        }catch(\Exception $e){
+            $response["status"]=0;
+            $response["msg"]=$e->getMessage();
+        }
+        return response()->json($response);
+    }
+
+    public function passRecovery(Request $req){ //Pide: nickname
+        $jdata = $req->getContent();
+        $data = json_decode($jdata);
+
+        $response["status"]= 1;
+        try{
+            if(isset($data->nickname)){
+                $user = Usuario::where('nickname', $data->nickname)->first();
+                if(!isset($user)){
+                    throw new Exception("Nickname no existe");
+                }
+                $newPass = Str::random(16);
+                $user->password = Hash::make($newPass);
+                $user->api_token = NULL;
+                $user->save();
+                $response["msg"]="Contraseña cambiada. Vuelve a iniciar sesión.";
+                $response["Password"]=$newPass;
+            }else{
+                throw new Exception("No has introducido nickname");
+            }
+        }catch(\Exception $e){
+            $response["status"]=0;
+            $response["msg"]=$e->getMessage();
+        }
+        return response()->json($response);
+    }
+
+}
---
+++ app/Http/Controllers/VentasController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+use App\Models\Carta;
+use App\Models\Venta;
+use Exception;
+
+class VentasController extends Controller
+{
+
+    public function crearVenta(Request $req){ //Pide: api_token, carta_id, quantity, price
+        $jdata = $req->getContent();
+        $data = json_decode($jdata);
+
+        $response["status"]=1;
+        try{
+            if(isset($data->carta_id) && isset($data->quantity) && isset($data->price)){
+                $user = Usuario::where('api_token', $data->api_token)->first();
+                $carta = Carta::find($data->carta_id);
+                if($user == null){
+                    throw new Exception("Error: Usuario no encontrado");
+                }
+                if($carta == null){
+                    throw new Exception("Error: Carta no encontrada");
+                }
+                if($data->quantity < 1){
+                    throw new Exception("Error: Vende al menos una carta");
+                }
+                if($data->price < 0.01){
+                    throw new Exception("Error: Precio mínimo es 0.01€");
+                }
+                $articulo = new Venta;
+                $articulo->usuario_id = $user->id;
+                $articulo->carta_id = $data->carta_id;
+                $articulo->name = $carta->name;
+                $articulo->quantity = $data->quantity;
+                $articulo->price = $data->price;
+                $articulo->save();
+            }else{
+                throw new Exception("Error: introduce api_token, usuario_id, carta_id, quantity, price");
+            }
+        }catch(\Exception $e){
+            $response["status"]=0;
+            $response["msg"]=$e->getMessage();
+        }
+        return response()->json($response);
+    }
+
+    public function buscarCartas(Request $req){ //Pide: api_token y name
+        $jdata = $req->getContent();
+        $data = json_decode($jdata);
+
+        $response["status"]=1;
+        try{
+            if(isset($data->name)){
+                $listaNombres = Carta::pluck('name','id')->toArray();
+                $listaRespuesta = [];
+                $listaRespuesta2 = [];
+                foreach ($listaNombres as $key => $nombreCompleto) {
+                    if(str_contains($nombreCompleto, $data->name)){
+                        $response["msg"]="Cartas encontradas";
+                        $coincidencia = Carta::where('name',$nombreCompleto)->first();
+                        $listaRespuesta["id"] = $coincidencia->id;
+                        $listaRespuesta["nombre"] = $coincidencia->name;
+                        array_push($listaRespuesta2, $listaRespuesta);
+                    }
+                }
+                if(!isset($coincidencia)){
+                    $response["msg"] = "No hay ninguna coincidencia";
+                }else{
+                    $response["coincidencias"] = $listaRespuesta2;
+                }
+            }else{
+                throw new Exception("Error: Introduce un nombre de una carta (name)");
+            }
+        }catch(\Exception $e){
+            $response["status"]=0;
+            $response["msg"]=$e->getMessage();
+        }
+        return response()->json($response);
+    }
+    
+    public function buscarAnuncio(Request $req){ //Pide: nombre
+        $jdata = $req->getContent();
+        $data = json_decode($jdata);
+
+        $response["status"]=1;
+        try{
+            if(isset($data->name)){
+                $listaNombres = Venta::pluck('name');
+                $coincidencias = [];
+                $response["msg"] = "No hay ninguna coincidencia";
+                foreach ($listaNombres as $key => $nombreCompleto) {
+                    if(str_contains($nombreCompleto, $data->name)){
+                        $response["msg"]="Articulos encontrados";
+                        array_push($coincidencias, Venta::where('name', $nombreCompleto)->first());
+                        //probar si se bugea con cartas que comparten parte del nombre
+                    }
+                }
+                
+                usort($coincidencias, function($object1, $object2) {
+                    return $object1->price > $object2->price;
+                });
+
+                $listaRespuesta = [];
+                $listaRespuesta2 = [];
+                foreach ($coincidencias as $key => $anuncio) {
+                    $listaRespuesta["id_anuncio"] =  $anuncio->id;
+                    $listaRespuesta["id_carta"] =  $anuncio->carta_id;
+                    $listaRespuesta["Carta"] =  $anuncio->name;
+                    $listaRespuesta["Cantidad"] = $anuncio->quantity;
+                    $listaRespuesta["Precio"] = $anuncio->price;
+                    $listaRespuesta["Vendedor"] = (Usuario::find($anuncio->usuario_id))->nickname;
+                    $listaRespuesta["id_vendedor"] =  $anuncio->usuario_id;
+                    array_push($listaRespuesta2, $listaRespuesta);
+                }
+                $response["coincidencias"] = $listaRespuesta2;
+            }else{
+                throw new Exception("Error: Introduce un nombre de una carta (name)");
+            }
+        }catch(\Exception $e){
+            $response["status"]=0;
+            $response["msg"]=$e->getMessage();
+        }
+        return response()->json($response);
+    }
+
+}
